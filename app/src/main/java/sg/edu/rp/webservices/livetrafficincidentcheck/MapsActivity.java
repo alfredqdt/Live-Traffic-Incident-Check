d5a33c38185778925d41f2f359ddaf9ce957cef6
19041582-Qayyum-Danish-Tan <19041582@myrp.edu.sg>
2021-09-06 19:31:48 +0800

    update

--- app/src/main/java/sg/edu/rp/webservices/livetrafficincidentcheck/MapsActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/livetrafficincidentcheck/MapsActivity.java
@@ -59,20 +59,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         if (lat != 0.0 && lng != 0.0) {
+
+            UiSettings uiCompass = mMap.getUiSettings();
+            uiCompass.setCompassEnabled(true);
+
+            UiSettings uiControlZoom = mMap.getUiSettings();
+            uiControlZoom.setZoomControlsEnabled(true);
+
             LatLng singapore = new LatLng(lat, lng);
             mMap.addMarker(new MarkerOptions().position(singapore).title(type));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(singapore,
