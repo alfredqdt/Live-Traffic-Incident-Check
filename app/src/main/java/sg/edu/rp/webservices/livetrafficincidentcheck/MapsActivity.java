b2fe28be065287a1c3eed82cb48c015179fdc052
19041582-Qayyum-Danish-Tan <19041582@myrp.edu.sg>
2021-09-06 22:25:44 +0800

    update

--- app/src/main/java/sg/edu/rp/webservices/livetrafficincidentcheck/MapsActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/livetrafficincidentcheck/MapsActivity.java
@@ -1,15 +1,14 @@
 package sg.edu.rp.webservices.livetrafficincidentcheck;
 
+
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,8 +23,6 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 import sg.edu.rp.webservices.livetrafficincidentcheck.databinding.ActivityMapsBinding;
@@ -42,14 +39,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-         binding = ActivityMapsBinding.inflate(getLayoutInflater());
-         setContentView(binding.getRoot());
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         Intent intent = getIntent();
         type = intent.getStringExtra("type");
         message = intent.getStringExtra("message");
-        lat = intent.getDoubleExtra("latitude",0.0);
-        lng = intent.getDoubleExtra("longitude",0.0);
+        lat = intent.getDoubleExtra("latitude", 0.0);
+        lng = intent.getDoubleExtra("longitude", 0.0);
 
         client = new AsyncHttpClient();
 
@@ -75,8 +72,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.addMarker(new MarkerOptions().position(singapore).title(type));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(singapore,
                     15));
-        }
-        else {
+        } else {
             client.addHeader("AccountKey", "SDWn7IqOSryTM/h6AOHDcw==");
             client.get("http://datamall2.mytransport.sg/ltaodataservice/TrafficIncidents", new JsonHttpResponseHandler() {
                 @Override
@@ -111,6 +107,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             });
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
     }
 
     @Override
